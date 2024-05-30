452aa9249b6cbbfdb31c4f8279362c9cfe8dc3e3
mqr <maqirong2003@163.com>
2024-05-30 13:21:11 +0800

    message 2: The first functional_test

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,19 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.page_source
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('To-Do', self.browser.title), "browser title was: " + self.browser.title
+        self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main()
